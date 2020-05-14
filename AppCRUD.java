501eb55c996b128aa6b652af322c2997d30d6e28
victorleite40 <victorsalvetti@hotmail.com>
2020-05-14 00:30:54 -0300

    Updated main

--- AppCRUD.java
+++ AppCRUD.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class AppCRUD {
 
-    public static int toInt(final String string) {
+    public static int toInt(String string) {
         return Integer.parseInt(string);
     }
     public static void main(String[] args) throws Exception {
@@ -13,59 +13,51 @@ public class AppCRUD {
 
         DynamicList list = new DynamicList();
 
-        boolean stop = false;
-        while (!stop)
+        boolean runMenu = true;
+
+        while (runMenu)
         {
-            String input = scan.nextLine();
-            String [] inputHandled =  input.split(" ");
+            String [] input = scan.nextLine().split(" ");
 
-            
-
-            switch (inputHandled[0]) {
+            switch (input[0]) {
                 case "add":
-                    list.add(toInt(inputHandled[1]), inputHandled[2], inputHandled[3], inputHandled[4], inputHandled[5]);
+                    list.add(toInt(input[1]), input[2], input[3], input[4], input[5]);
                     break;
-
+                    
                 case "del":
-                    list.remove(toInt(inputHandled[1]));
+                    list.remove(toInt(input[1]));
                     break;
 
                 case "info":
-                    System.out.println(list.get( toInt(inputHandled[1]) ));
+                    System.out.println(list.get( toInt(input[1]) ));
+                    break;
+
+                case "query":
+                    System.out.println(list.get( toInt(input[1]) ));
                     break;
 
                 case "000":
-                    stop = true;
+                    runMenu = false;
                     break;
 
                 default:
                     break;
             }
         }
-        scan.close();
-        
-        /*
-        
-        list.add(0, "Roberto", "Carlos", "01/01/1960", "+55-11-0169-0369");
-        list.add(1, "Capitao", "Nascimento", "11/05/1979", "+55-21-0190-0190");
-        System.out.println(list.get(1));
-        
 
-         
-        list.add(10, 0);
-        System.out.println(list);
-        list.add(20, 0);
-        System.out.println(list);
-        list.add(30, 2);
-        System.out.println(list);
-        list.add(40, 1);
-        System.out.println(list);
-        System.out.println(list.exist(40));
-        System.out.println(list.exist(50));
-        list.add(50);
-        System.out.println(list.get(3));
-        System.out.println(list);
-        list.set(90,3);
+        scan.close();
+                
+        
+        /** 
+         * TESTS
+        */
+
+        /*
+        list.add(69, "Roberto", "Carlos", "01/01/1960", "+55-11-0169-0369");
+        list.add(190, "Capitao", "Nascimento", "11/05/1979", "+55-21-0190-0190");
+        //System.out.println(list.get(1));
+        System.out.println(list.get(190));
+
         */
     }
 }
