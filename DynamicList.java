63dd07091f5aeb529848d31a401a6d94c13e0f96
victorleite40 <victorsalvetti@hotmail.com>
2020-05-26 18:55:47 -0300

    Updated class

--- DynamicList.java
+++ DynamicList.java
@@ -1,24 +1,32 @@
 package crudEstrutura;
 
-public class DynamicList {
+import java.util.ArrayList;
+import java.util.List;
+
+public class DynamicList 
+{
     private Person start;
     private Person end;
     private int amount;
 
-    public DynamicList() {
+    public DynamicList() 
+    {
         start = end = null;
         amount = 0;
     }
 
-    public int size() {
+    public int size() 
+    {
         return amount;
     }
     
-    public boolean isEmpty() {
+    public boolean isEmpty() 
+    {
         return start==null;
     }
 
-    public int getPosById(int id) {
+    public int getPosById(int id)
+    {
         Person current = start;
         
         for (int i=0; i<size(); i++) {
@@ -28,7 +36,8 @@ public class DynamicList {
         return -1;
     }
 
-    public boolean existId(int id) {
+    public boolean existId(int id) 
+    {
         if (!isEmpty()) {
             Person current = start;
             while (current!=null) {
@@ -39,7 +48,8 @@ public class DynamicList {
         return false;
     }
 
-    public void add(int id, String firstName, String lastName, String birthDay, String phoneNumber, int position) {
+    public void add(int id, String firstName, String lastName, String birthDay, String phoneNumber, int position) 
+    {
         if (position<=size() && position>-1) {
             Person newNode = new Person(id, firstName, lastName, birthDay, phoneNumber);
             
@@ -65,7 +75,8 @@ public class DynamicList {
         }
     }
 
-    public void add(int id, String firstName, String lastName, String birthDay, String phoneNumber) throws Exception {
+    public void add(int id, String firstName, String lastName, String birthDay, String phoneNumber) throws Exception 
+    {
         if (!existId(id)) {
             add
             (
@@ -82,7 +93,8 @@ public class DynamicList {
         }
     }
 
-    public void remove(int id) throws Exception {
+    public void remove(int id) throws Exception 
+    {
         int position = getPosById(id);
         
         if (position>-1) {
@@ -106,7 +118,8 @@ public class DynamicList {
                     amount--;
                     
                 } catch (NullPointerException e) {
-                    System.err.println("ID " + id + " não encontrado.");
+                    System.err.println("ID " + id + " não encontrado."); // TESTAR
+                    // throw new Exception( "ID " + id + " não encontrado." );
                 }
             }
         } else {
@@ -114,7 +127,8 @@ public class DynamicList {
         }
     }
 
-    public void set(String newData, int id) {
+    public void set(String newData, int id) 
+    {
         int position = getPosById(id);
 
         if (position<size() && position>-1) {
@@ -127,7 +141,8 @@ public class DynamicList {
         }
     }
 
-    public String get(int id) throws Exception{
+    public String get(int id) throws Exception
+    {
         try {
             int position = getPosById(id);
             Person current = start;
@@ -142,33 +157,79 @@ public class DynamicList {
         }   
     }
 
-    public String query(String input[]) {
+    public int search(String arg1[], String arg2[], String arg3[], String arg4[])
+    {
+        if (!isEmpty()) {
+            Person current = start;
+            while (current!=null) {
+                if(current.id==id) return true;
+                current = current.next;
+            }
+        }
+        return 1;
+    }
+
+    public String query(String input[]) 
+    {
         String dataString = "";
 
         Person current = start;
 
+        if (input.length==1) {
+            return "Argumentos insulficientes.";
+        } else {
+            if (input.length==2) 
+            {
+                String arg1[] = input[1].split(":");
 
-        for (int i=0; i<input.length-1; i++) {
-            input[i] = input[i+1];
-            String args[] = input[i].split(":");
+                while (current!=null)
+                {
+                    if (arg1[1].equals(current.firstName) || arg1[1].equals(current.lastName) || arg1[1].equals(current.birthDay) || arg1[1].equals(current.phoneNumber))
+                    {
+                        dataString += current.id + " ";
+                    }
+                    current = current.next;
+                }                
+            }
+            else if (input.length==3)
+            {
+                String arg1[] = input[1].split(":");
+                String arg2[] = input[2].split(":");
+                
+                while (current!=null)
+                {
+                    if (arg1[1].equals(current.firstName) || arg1[1].equals(current.lastName) || arg1[1].equals(current.birthDay) || arg1[1].equals(current.phoneNumber)
+                        &&
+                        arg2[1].equals(current.firstName) || arg2[1].equals(current.lastName) || arg2[1].equals(current.birthDay) || arg2[1].equals(current.phoneNumber))
+                    {
+                        dataString += current.id + " ";
+                    }
+                    current = current.next;
+                }        
+
+            }
+            else if (input.length==4)
+            {
+                String arg3[] = input[3].split(":");
+            }
+            else if (input.length==5)
+            {
+                String arg4[] = input[4].split(":");
+            }
         }
 
-        /*
-        while (current!=null) {
-            //if(current.id==id) return true;
-            current = current.next;
-        }
-        */
         return dataString;
     }
 
-    @Override public String toString() {
+    @Override public String toString() 
+    {
         String dataString = "";
         Person temp = start;
 
-        for (int i = 0; temp!=null; i++) {
+        for (int i = 0; temp!=null; i++) 
+        {
             dataString += (i>0) ? "\n" : "" ;
-            dataString += "[" + temp.id + "] "  + temp.firstName + " " + temp.ln + " " + temp.birthDay + " " + temp.phoneNumber;
+            dataString += "[" + temp.id + "] "  + temp.firstName + " " + temp.lastName + " " + temp.birthDay + " " + temp.phoneNumber;
             temp = temp.next;
         }
 
