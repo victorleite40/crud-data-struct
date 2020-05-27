9b8688a7505bb69327c7684fafeefd0fea768cd4
victorleite40 <victorsalvetti@hotmail.com>
2020-05-26 23:42:16 -0300

    Updated main

--- AppCRUD.java
+++ AppCRUD.java
@@ -21,20 +21,20 @@ public class AppCRUD {
         {
             String input[] = scan.nextLine().split(" ");
 
-            //try {
+            try {
                 switch (input[0]) {
                     case "add":
-                        list.add(toInt(input[1]), input[2], input[3], input[4], input[5]);
+                        System.out.print( list.add(toInt(input[1]), input[2], input[3], input[4], input[5]) );
                         // "add <id> <first_name> <last_name> <birtday> <phone_number>";
                         break;
                         
                     case "del":
-                        list.remove(toInt(input[1]));
+                        System.out.print( list.remove(toInt(input[1])) );
                         // "del <id>";
                         break;
 
                     case "info":
-                        System.out.println(list.get( toInt(input[1]) ));
+                        System.out.print(list.get( toInt(input[1]) ));
                         // "info <id>";
                         break;
 
@@ -45,32 +45,15 @@ public class AppCRUD {
                     case "000":
                         runMenu = false;
                         break;
-                        
-                    case "list": // DEV
-                        System.out.println(list);
-                        break;
 
                     default:
                         break;
                 }
-            /*} catch (ArrayIndexOutOfBoundsException e) {
+            } catch (ArrayIndexOutOfBoundsException e) {
                 System.err.println("Argumentos insuficientes.");
-            }*/
+            }
         }
 
         scan.close();
-                
-        
-        /** 
-         * TESTS
-        */
-
-        /*
-        list.add(69, "Roberto", "Carlos", "01/01/1960", "+55-11-0169-0369");
-        list.add(190, "Capitao", "Nascimento", "11/05/1979", "+55-21-0190-0190");
-        //System.out.println(list.get(1));
-        System.out.println(list.get(190));
-
-        */
     }
 }
